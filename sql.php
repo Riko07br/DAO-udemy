67d3fdefe1787fd457f1975590b6c612bcd73336
Riko07 <f.natanael.felix.b@gmail.com>
2020-05-18 14:32:40 -0300

    data oriented objects

---
+++ sql.php
@@ -0,0 +1,42 @@
+<?php
+class Sql extends PDO {
+    
+    private $conn;    
+    
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", ""); 
+    }
+    
+    //prepara diversos parametros
+    private function setParams($statement, $parameters = array()){
+        foreach ($parameters as $key => $value) {
+            $this->setParameter($statement, $key, $value);
+        }
+    }
+    
+    //prepara e trata um unico parametro
+    private function setParam($statement, $key, $value){
+        $statement->bindParam($key, $value);
+    }
+    
+    //metodo para inserir query
+    public function query($rawQuery, $params = array()) {
+        
+        $stmt = $this->conn->prepare($rawQuery);
+        
+        $this->setParams($stmt, $params);
+        
+         $stmt->execute();
+         
+         return $stmt;
+    }
+    
+    //retorna 
+    public function select($rawQuery, $params = array()):array {
+        $stmt = $this->query($rawQuery, $params);
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    
+}
