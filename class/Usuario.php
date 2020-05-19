c22fb6f4949ab40cc1b1bcc526ecebfcdcdb20f4
Riko07 <f.natanael.felix.b@gmail.com>
2020-05-19 10:20:02 -0300

    estufando mais usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -51,6 +51,7 @@ class Usuario {
     public function setDtcadastro($dtcadastro){
         $this->dtcadastro = $dtcadastro;
     }
+    
     ///////////////////////////////////////////////
     
     public function loadById($id) {
@@ -67,6 +68,37 @@ class Usuario {
             $this->setDtcadastro(new DateTime($row["dtcadastro"]));
         }
     }
-        
     
+    //como não tem o 'this' pode ser um metodo solto
+    //não precisando instanciar um 'usuario' para chama-lo
+    public static function getList(){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario;");
+    }
+    
+    public static function search($login) {
+        $sql = new Sql();
+        
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY idusuario", array(":SEARCH" => "%".$login."%"));
+    }
+    
+    public function login($login, $password){
+        $sql =  new Sql();
+        
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(
+            ":LOGIN"=>$login,
+            ":SENHA"=>$password));
+        
+        if(count($result) > 0){
+            $row = $result[0];
+            
+            $this->setIdusuario($row["idusuario"]);
+            $this->setDeslogin($row["deslogin"]);
+            $this->setDessenha($row["dessenha"]);
+            $this->setDtcadastro(new DateTime($row["dtcadastro"]));
+        }else {
+            
+            throw new Exception("Login ou senha tão errado");
+        }
+    }
 }
--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 
 require_once ("config.php");
 
+//chamar por id
 $usuario = new Usuario();
+//$usuario->loadById(3);
+//echo $usuario;
 
-$usuario->loadById(3);
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// $search = Usuario::search("r");
+// echo json_encode($search);
+
+$usuario->login("bob", "bob");
 
 echo $usuario;
